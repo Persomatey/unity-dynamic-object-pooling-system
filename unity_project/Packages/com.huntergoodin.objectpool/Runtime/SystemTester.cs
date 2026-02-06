f0b3eb94ebc64b5b860b5d83c925aad3f837cf82
Hunter Goodin <huntergoodin1@hotmail.com>
2026-02-06 10:34:51 -0800

    Finished sample scene for GameObjects

--- unity_project/Packages/com.huntergoodin.objectpool/Runtime/SystemTester.cs
+++ unity_project/Packages/com.huntergoodin.objectpool/Runtime/SystemTester.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SystemTester : MonoBehaviour
 {
 	[SerializeField] private GameObject projectile;
+	[SerializeField] private GameObject vfx;
 
 	[SerializeField] private TextMeshProUGUI projectilesActive; 
 	[SerializeField] private TextMeshProUGUI projectilesInactive;
@@ -24,27 +25,29 @@ public class SystemTester : MonoBehaviour
 		projectilesActive.text = $"Active Projectiles: {ObjectPoolManager.Instance.ObjectPoolActiveSize(ObjectPoolManager.PoolType.Projectiles, projectile)}";
 		projectilesInactive.text = $"Inactive Projectiles: {ObjectPoolManager.Instance.ObjectPoolInactiveSize(ObjectPoolManager.PoolType.Projectiles, projectile)}";
 		projectilesTotal.text = $"Total Projectiles: {ObjectPoolManager.Instance.ObjectPoolTotalSize(ObjectPoolManager.PoolType.Projectiles, projectile)}";
+
+		vfxActive.text = $"Active VFX: {ObjectPoolManager.Instance.ObjectPoolActiveSize(ObjectPoolManager.PoolType.VFX, vfx)}";
+		vfxInactive.text = $"Inactive VFX: {ObjectPoolManager.Instance.ObjectPoolInactiveSize(ObjectPoolManager.PoolType.VFX, vfx)}";
+		vfxTotal.text = $"Total VFX: {ObjectPoolManager.Instance.ObjectPoolTotalSize(ObjectPoolManager.PoolType.VFX, vfx)}";
 	}
 
 	public void SpawnProjectile()
 	{
-		GameObject obj = ObjectPoolManager.Instance.SpawnObject(projectile, transform.position, transform.rotation, ObjectPoolManager.PoolType.Projectiles);
+		// Spawn projectile 
+		GameObject projObj = ObjectPoolManager.Instance.SpawnObject(projectile, transform.position, transform.rotation, ObjectPoolManager.PoolType.Projectiles); // This basically replaces GameObject.Instantiate() 
+		Vector3 randomDirection = Quaternion.AngleAxis(Random.Range(0f, 25), Random.onUnitSphere) * transform.forward;
+		projObj.GetComponent<Rigidbody>().linearVelocity = randomDirection * 10; 
+		StartCoroutine(DelayedDespawnObject(projObj, ObjectPoolManager.PoolType.Projectiles, 1f));
 
-		Vector3 randomDirection =
-			Quaternion.AngleAxis(
-				Random.Range(0f, 25),
-				Random.onUnitSphere
-			) * transform.forward;
-
-		obj.GetComponent<Rigidbody>().linearVelocity = randomDirection * 10; 
-
-		StartCoroutine(DelayedDespawnBall(obj)); 
+		// Spawn VFX 
+		GameObject vfxObj = ObjectPoolManager.Instance.SpawnObject(vfx, transform.position, transform.rotation, ObjectPoolManager.PoolType.Projectiles);
+		vfxObj.GetComponent<ParticleSystem>().Play();  
+		StartCoroutine(DelayedDespawnObject(vfxObj, ObjectPoolManager.PoolType.VFX, 0.5f));
 	}
 
-	private IEnumerator DelayedDespawnBall(GameObject obj)
+	private IEnumerator DelayedDespawnObject(GameObject obj, ObjectPoolManager.PoolType poolType, float time)
 	{
-		yield return new WaitForSeconds(1f);
-
-		ObjectPoolManager.Instance.ReturnObjectToPool(obj, ObjectPoolManager.PoolType.Projectiles); 
+		yield return new WaitForSeconds(time); // Slight delay to show the objects before they're re-pooled 
+		ObjectPoolManager.Instance.ReturnObjectToPool(obj, poolType); // This basically replaces Destroy(GameObject) 
 	}
 }
